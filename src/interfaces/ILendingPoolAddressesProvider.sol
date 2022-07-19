287bb33f697b945922ceb0b20bd00e22f91cbd27
Rajath Alex <rajathalex@gmail.com>
2022-07-19 17:50:32 -0400

    Delete tests and interfaces

--- src/interfaces/ILendingPoolAddressesProvider.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.11;
-
-interface ILendingPoolAddressesProvider {
-    function getLendingPoolConfigurator() external returns (address);
-
-    function getPriceOracle() external view returns (address);
-}
--- src/interfaces/IPriceOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.11;
-
-interface IPriceOracle {
-    function setAssetSources(address[] calldata assets, address[] calldata sources) external;
-
-    function getSourceOfAsset(address asset) external view returns (address);
-}
